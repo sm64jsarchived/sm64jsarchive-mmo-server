a8fba886dc1ad3956b8510bfc7c92773f2a9d5ec
GManTheMarioGuy <155935644+GManTheMarioGuy@users.noreply.github.com>
2025-08-21 21:13:38 -0500

    Oops
    forgot to change this

--- publish-server.sh
+++ publish-server.sh
@@ -7,7 +7,7 @@ echo "using git hash $GIT_HASH"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker buildx build -t sm64jsarchive-mmo-server .
-docker tag sm64jsarchive gmanthemarioguy/sm64jsarchive-mmo-server:latest
+docker tag sm64jsarchive-mmo-server gmanthemarioguy/sm64jsarchive-mmo-server:latest
 docker push gmanthemarioguy/sm64jsarchive-mmo-server:latest
-docker tag sm64jsarchive gmanthemarioguy/sm64jsarchive-mmo-server:$GIT_HASH
+docker tag sm64jsarchive-mmo-server gmanthemarioguy/sm64jsarchive-mmo-server:$GIT_HASH
 docker push gmanthemarioguy/sm64jsarchive-mmo-server:$GIT_HASH
