ccdaf3d40d5d8eea30324e101a61e4b2a0c8761e
GManTheMarioGuy <155935644+GManTheMarioGuy@users.noreply.github.com>
2025-08-21 21:43:26 -0500

    epic (publish)

--- publish.sh
+++ publish.sh
@@ -6,8 +6,8 @@ echo "using git hash $GIT_HASH"
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker buildx build -t sm64jsarchive .
-docker tag sm64jsarchive gmanthemarioguy/sm64jsarchive:latest
+docker pull gmanthemarioguy/sm64jsarchive || true
+docker build --cache-from=gmanthemarioguy/sm64jsarchive -t gmanthemarioguy/sm64jsarchive .
 docker push gmanthemarioguy/sm64jsarchive:latest
-docker tag sm64jsarchive gmanthemarioguy/sm64jsarchive:$GIT_HASH
+docker tag gmanthemarioguy/sm64jsarchive gmanthemarioguy/sm64jsarchive:$GIT_HASH
 docker push gmanthemarioguy/sm64jsarchive:$GIT_HASH
